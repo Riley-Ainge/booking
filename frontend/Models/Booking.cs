c8053c7e9b374acf3649becbda4938a633f897d3
Riley-Ainge <117593905+Riley-Ainge@users.noreply.github.com>
2022-11-07 02:45:41 +0800

    Add files via upload

---
+++ frontend/Models/Booking.cs
@@ -0,0 +1,11 @@
+﻿namespace Assignment2.Models
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Nullable<System.DateTime> Start { get; set; }
+        public Nullable<System.DateTime> End { get; set; }
+        public Nullable<int> CentreID { get; set; }
+    }
+}
---
+++ frontend/Models/Centre.cs
@@ -0,0 +1,8 @@
+﻿namespace Assignment2.Models
+{
+    public class Centre
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
